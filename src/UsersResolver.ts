8b503fa238187494c0ac0bc8de2aff840f68f6da
Lorris Lehuede <grubux@pop-os.localdomain>
2021-10-29 10:56:24 +0200

    fix: eslint/ts errors

--- src/UsersResolver.ts
+++ src/UsersResolver.ts
@@ -5,6 +5,7 @@ import UsersModel from './models/Users';
 @Resolver()
 class UsersResolver {
   @Query(() => [User])
+  // eslint-disable-next-line class-methods-use-this
   async allUsers() {
     const getAllUsers = await UsersModel.find();
     return getAllUsers;
