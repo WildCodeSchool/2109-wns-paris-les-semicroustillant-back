44a47353748cfd208e071f5a650f9aec8779de0a
Fredy <fredy.ly.wcs@gmail.com>
2021-10-29 11:32:04 +0200

    feat: adding create method

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -1,12 +1,12 @@
 import { Schema, model } from 'mongoose';
 
 const UsersSchema = new Schema({
-  firstname: { type: String },
-  lastname: { type: String },
+  firstname: String,
+  lastname: String ,
   email: { type: String, unique: true },
   hash: { type: String, unique: true },
-  role: { type: String },
-  position: { type: String },
+  role: String,
+  position: String,
 });
 const UsersModel = model('users', UsersSchema);
 export default UsersModel;
