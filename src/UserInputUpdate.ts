91461afad133ca0b68065a60274af9b44348e36e
Fredy <fredy.ly.wcs@gmail.com>
2021-11-23 17:25:22 +0100

    WIP - UpdateUser

---
+++ src/UserInputUpdate.ts
@@ -0,0 +1,25 @@
+import { InputType, Field } from "type-graphql";
+
+@InputType()
+export default class UserInput {
+  @Field()
+  id: string;
+
+  @Field()
+  firstname?: string;
+
+  @Field()
+  lastname?: string;
+
+  @Field()
+  email?: string;
+
+  @Field()
+  hash?: string;
+
+  @Field()
+  role?: string;
+
+  @Field()
+  position?: string;
+}
