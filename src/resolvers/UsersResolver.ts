64a71687de6a6649f938408ba2bf7c36430686fa
Fredy <fredy.ly.wcs@gmail.com>
2021-11-23 19:16:55 +0100

    updating a user working

--- src/resolvers/UsersResolver.ts
+++ src/resolvers/UsersResolver.ts
@@ -12,80 +12,41 @@ class UsersResolver {
   @Query(() => [User])
   async allUsers() {
     const getAllUsers = await UsersModel.find();
+
     return getAllUsers;
   }
 
-  @Mutation(() => String)
+  @Mutation(() => User)
   async addUser(@Arg('userInput') userInput: UserInput) {
     await UsersModel.init();
     const user = await UsersModel.create(userInput);
     await user.save();
-    return 'User added';
+
+    return user;
   }
 
-  // Model.findByIdAndUpdate(id, { name: 'jason bourne' }, options, callback)
-
-  // @Mutation(() => String)
-  // async updateUser(
-  //   @Arg('id') userId: IdInput,
-  //   @Arg('userInput') userInput: UserInput) {
-  //     try {
-  //       const user = await UsersModel.findByIdAndUpdate(
-  //         userId, userInput,
-  //       { new: true });
-  //       console.log('USER :', user);
-  //       // await user.save();
-  //       // return 'User successfully updated';
-  //       return user;
-        
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   return UsersModel.findOne(userId);
-  // }
-
-  @Mutation(() => String)
+  @Mutation(() => User)
   async updateUser(
     @Arg('id', () => String) userId: IdInput,
     @Arg('userInputUpdate') userInputUpdate: UserInputUpdate) {
       try {
-        const user = await UpdateUserModel.findByIdAndUpdate(
-          userId, userInputUpdate,
-        { new: true });
-        console.log('USER :', user);
-        // await user.save();
-        // return 'User successfully updated';
+        const user = await UpdateUserModel.findByIdAndUpdate(userId, userInputUpdate);
+
         return user;
-        
       } catch (err) {
         console.log(err);
       }
-    return UpdateUserModel.findOne(userId);
+
+    return UsersModel.findOne(userId);
   }
 
   @Mutation(() => String)
   async deleteUser(@Arg('id', () => String) id: IdInput) {
     await UsersModel.init();
     await UsersModel.findByIdAndRemove(id);
-    return 'User deleted';
-  }
 
-  
+    return 'User deleted';
+  }  
 }
 
 export default UsersResolver;
-
-
-/* router.route("/update").post(function(req, res) {
-  kennels.findByIdAndUpdate(
-    { _id: "5db6b26730f133b65dbbe459" },
-    { breed: "Great Dane" },
-    function(err, result) {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send(result);
-      }
-    }
-  );
-}); */
