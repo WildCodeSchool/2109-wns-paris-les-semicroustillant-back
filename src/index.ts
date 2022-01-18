823673a16572897fd3f8f664b15b352115f51d1d
SophieTopart <sophiebailly9@gmail.com>
2022-01-18 12:04:33 +0100

    Tests

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,12 @@ import createConnection from './createConnection';
 import createServer from './server';
 
 async function start() {
-  
   try {
     const db = 'semidb';
 
     console.log('awaiting for database connection');
     await createConnection(`mongodb://127.0.0.1:27017/${db}`);
-    console.log('connected to database');
+    console.log(`connected to database ${db}`);
     const server = await createServer();
 
     // Start the server
