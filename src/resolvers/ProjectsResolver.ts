7333feb047f4f871e4fb1ee6abaf8ad4aa8059bf
SophieTopart <77396708+SophieTopart@users.noreply.github.com>
2022-02-25 10:52:02 +0100

    Feat/projects (#35)
    * Added status and description fields to Projects
    
    * Added advancement field in Projects
    
    * Tests modified
    
    * Advancement in progess
    
    * Fix schema projectId
    
    * In progress
    
    * Advancement for getOneProject done
    
    * Get projects advancement done

--- src/resolvers/ProjectsResolver.ts
+++ src/resolvers/ProjectsResolver.ts
@@ -2,9 +2,11 @@
 import { Arg, Query, Resolver, Mutation } from 'type-graphql';
 import Project from '../entities/Projects';
 import ProjectModel from '../models/ProjectModel';
+import TicketsModel from '../models/Tickets';
 import ProjectInput from '../inputs/ProjectInput';
 import ProjectInputUpdate from '../inputs/ProjectInputUpdate';
 import IdInput from '../inputs/IdInput';
+import { getAdvancement } from './TicketsResolver';
 
 @Resolver()
 class ProjectsResolver {
@@ -12,6 +14,34 @@ class ProjectsResolver {
   async getAllProjects() {
     try {
       const getAllProjects = await ProjectModel.find();
+
+      const getAllTickets = await TicketsModel.find();
+
+      for (let i = 0; i < getAllProjects.length; i += 1) {
+        const getCorrespondingTickets = getAllTickets.filter(
+          (ticket) => ticket.projectId === getAllProjects[i]._id.toString()
+        );
+
+        for (let j = 0; j < getCorrespondingTickets.length; j += 1) {
+          getCorrespondingTickets[j].advancement = getAdvancement(
+            getCorrespondingTickets[j]
+          );
+        }
+
+        const getTicketsAdvancements: number[] = [];
+
+        for (let k = 0; k < getCorrespondingTickets.length; k += 1) {
+          getTicketsAdvancements.push(getCorrespondingTickets[k].advancement);
+        }
+
+        const sumOfTicketsAdvancements = getTicketsAdvancements.reduce(
+          (a, b) => a + b,
+          0
+        );
+
+        getAllProjects[i].advancement =
+          sumOfTicketsAdvancements / getCorrespondingTickets.length;
+      }
 
       if (!getAllProjects) {
         throw new Error('Cannot find any project');
@@ -27,6 +57,30 @@ class ProjectsResolver {
   async getOneProject(@Arg('projectId', () => String) projectId: IdInput) {
     try {
       const getOneProject = await ProjectModel.findById(projectId);
+
+      const getCorrespondingTickets = await TicketsModel.find({
+        projectId: getOneProject._id,
+      });
+
+      for (let i = 0; i < getCorrespondingTickets.length; i += 1) {
+        getCorrespondingTickets[i].advancement = getAdvancement(
+          getCorrespondingTickets[i]
+        );
+      }
+
+      const getTicketsAdvancements: number[] = [];
+
+      for (let i = 0; i < getCorrespondingTickets.length; i += 1) {
+        getTicketsAdvancements.push(getCorrespondingTickets[i].advancement);
+      }
+
+      const sumOfTicketsAdvancements = getTicketsAdvancements.reduce(
+        (a, b) => a + b,
+        0
+      );
+
+      getOneProject.advancement =
+        sumOfTicketsAdvancements / getCorrespondingTickets.length;
 
       if (!getOneProject) {
         throw new Error('Cannot find this project');
