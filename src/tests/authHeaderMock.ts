afb59ad6abb46786a764aea481632611e077e576
Fredy <fredy.ly.wcs@gmail.com>
2022-04-26 19:36:37 +0200

    adding authHeaderMock file

---
+++ src/tests/authHeaderMock.ts
@@ -0,0 +1,33 @@
+import { ApolloServer, gql } from 'apollo-server';
+import UserModel from '../models/UserModel';
+
+const authHeaderMock = async (server: ApolloServer) => {
+    const userAdmin = {
+      firstname: 'admin-fn',
+      lastname: 'admin-ln',
+      email: 'admin@email.com',
+      hash: '$2b$10$Vvyb64CIVPOtVtDWFq/inu5Hg3EnpRI9ypCFidKh7BnRZG/gq/hP.', // in clear 'admin-hash',
+      role: 'admin',
+      position: 'Product Owner',
+    };
+
+    await new UserModel(userAdmin).save();
+
+    const loginUnitTest = gql`
+      query Login($hash: String!, $email: String!) {
+        login(password: $hash, email: $email)
+      }
+    `;
+
+    const res = await server.executeOperation({
+      query: loginUnitTest,
+      variables: {
+        hash: 'admin-hash',
+        email: 'admin@email.com',
+      },
+    });
+
+    return res.data?.login;
+  };
+
+  export default authHeaderMock;
