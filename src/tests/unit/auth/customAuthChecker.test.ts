53fd0faa95a59a7200d0ca0d4239982a7168afdd
Fredy <fredy.ly.wcs@gmail.com>
2022-06-22 16:31:38 +0200

    fix uni test with context

--- src/tests/unit/auth/customAuthChecker.test.ts
+++ src/tests/unit/auth/customAuthChecker.test.ts
@@ -1,43 +1,39 @@
-// import { AuthChecker } from 'type-graphql';
-import jwt from 'jsonwebtoken';
-// import jwt, { Secret, JwtPayload } from 'jsonwebtoken';
+import jwt, { Secret } from 'jsonwebtoken';
 import UserModel from '../../../models/UserModel';
 import customAuthChecker from '../../../auth/customAuthChecker';
 
 describe('customAuthChecker', () => {
-  // @SophieTopart: how to correctly type context and process?
-  let context: { token: string } | null = null;
-  let roles: string[] | null = null;
-  // let process: { env: { SECRET_JWT_KEY: Secret } } | null = null;
+  let context: { token: string };
+  let root: any;
+  let args: any;
+  let info: any;
+  let roles: string[];
+  process.env.SECRET_JWT_KEY as Secret;
 
   beforeEach(() => {
     context = {
       token: 'fake-token',
     };
+    root = 'root';
+    args = 'args';
+    info =  'info';
     roles = ['admin'];
-    // process = {
-    //   env: {
-    //     SECRET_JWT_KEY: 'fake-secret',
-    //   },
-    // };
+    process.env.SECRET_JWT_KEY = 'fake-secret';
   });
 
   it('should successfully check the user', async () => {
-    // const userJwt = context?.token;
-    // const secret = process?.env.SECRET_JWT_KEY;
     jwt.verify = jest.fn().mockReturnValue({ userId: 'user-id' });
     UserModel.findOne = jest
       .fn()
       .mockResolvedValue({ email: 'user@email.com', role: 'admin' });
 
-    // @SophieTopart: typing issue with customAuthChecker
-    // @ts-ignore
-    const result = await customAuthChecker({ context }, roles);
-
-    // console.log(process?.env.SECRET_JWT_KEY);
+    const result = await customAuthChecker({ root, args, context, info }, roles);
 
     expect(result).toBe(true);
-    // expect(jwt.verify).toHaveBeenCalledWith(context?.token, process?.env.SECRET_JWT_KEY);
+    expect(jwt.verify).toHaveBeenCalledWith(
+      context?.token,
+      process.env.SECRET_JWT_KEY
+    );
     expect(UserModel.findOne).toHaveBeenCalledWith(
       { _id: 'user-id' },
       'email role'
