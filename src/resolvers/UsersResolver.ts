67f7d3b8bae4642af0a0db349e5382a79fd1ab71
Fredy <fredy.ly.wcs@gmail.com>
2022-07-08 10:31:22 +0200

    last fixes

--- src/resolvers/UsersResolver.ts
+++ src/resolvers/UsersResolver.ts
@@ -8,13 +8,6 @@ import UserInputUpdate from '../inputs/UserInputUpdate';
 import { adminsOnly } from '../auth/usersRole';
 // Available authorized:
 // roles adminsOnly = ['admin', 'super admin'] and superAdmin = ['super admin']
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-// import { IUser } from '../types/types';
->>>>>>> 348aeaf2e25a29d4f77dfabc02c023de249e9684
-=======
->>>>>>> sub-dev
 
 @Resolver()
 class UsersResolver {
